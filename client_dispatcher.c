deb80beacea0922648036256748f8e4aed9bde54
Gustaw Blachowski <gb448194@students.mimuw.edu.pl>
2024-03-24 15:54:22 +0100

    Add data structures.

--- client_dispatcher.c
+++ client_dispatcher.c
@@ -17,7 +17,7 @@ int main(int argc, char* argv[]) {
     if (argc != 4){
         fatal("usage: %s <protocol> <host> <port>", argv[0]);
     }
-    else if (strcmp(argv[1], TCP_PROT) != 0 && strcmp(argv[1], UDP_PROT) && 
+    else if (strcmp(argv[1], TCP_PROT) != 0 && strcmp(argv[1], UDP_PROT) &&
     strcmp(argv[1], UDPR_PROT) != 0) {
         fatal("Protocol %s is not supported.", argv[1]);
     }
--- common.h
+++ common.h
@@ -1,8 +1,59 @@
 #ifndef COMMON_H
 #define COMMON_H
 
+#include <inttypes.h>
+#include <stddef.h>
+#include <sys/types.h>
+
 #define TCP_PROT "tcp"
 #define UDP_PROT "udp"
 #define UDPR_PROT "udpr"
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+    uint8_t prot_id;
+    // Big endian
+    uint64_t data_length;
+} CONN;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+} CONACC;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+} CONRJT;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+    // Big endian
+    uint64_t pkt_nr;
+    // Big endian
+    uint32_t data_size;
+    const char* data;
+} DATA;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+    // Big endian
+    uint64_t pkt_nr;
+} ACC;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+    // Big endian
+    uint64_t pkt_nr;
+} RJT;
+
+typedef struct __attribute__((__packed__)) {
+    uint8_t pkt_type_id;
+    uint64_t session_id;
+} RCVD;
 
 #endif
